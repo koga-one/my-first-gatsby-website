6ad5e62443186e7644b158269aac4fa0b4e34808
André Koga <mailbykoga@gmail.com>
2021-12-31 11:30:47 -0300

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://koga.one",
-    title: "My Gatsby Site",
+    title: "KOGA.ONE",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
